aadbfca47cf333943eb42375f1bc8c9fe13a088e
jibran.ahmad <jibran.ahmad@nitb.gov.pk>
2024-05-31 20:31:34 +0500

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import Navbar from "./components/navbar";
 import Main from "./components/main";
 import Roadmap from "./components/roadmap";
+import { Web3Provider } from "./components/web3context";
+
 function App() {
   return (
-      <div className="bg-[url('./assets/full-bg.jpg')] font-bod">
-          
-        {/* [#0f172a] bg-[url('./assets/bg-image.png')] w-full h-screen  ...*/}
+    <div className="bg-[url('./assets/full-bg.jpg')] font-bod">
+      {/* [#0f172a] bg-[url('./assets/bg-image.png')] w-full h-screen  ...*/}
+      <Web3Provider>
         <Navbar />
 
         <Main />
 
         <Roadmap />
-        
-      </div>
-  )
+      </Web3Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,51 +1,57 @@
-import React from "react";
-import Web3 from 'web3';
-import { ethers } from 'ethers';
-import { useState } from 'react';
-import icon from'../assets/metmask-icon.png';
+import React, { useEffect, useState, useContext } from "react";
+import Web3 from "web3";
+import { ethers } from "ethers";
+import { useState } from "react";
+import icon from "../assets/metmask-icon.png";
+import { Web3Context } from "./web3context";
 
-export default function Navbar(){
+export default function Navbar() {
+  const { isConnectedd, web3, connectedAddress, connectToMetaMask } =
+    useContext(Web3Context);
 
-    const web3 = new Web3(window.ethereum);
-    const [userAccount, setUserAccount] = useState('');
-
-    async function connectWallet() {
-      try {
-        await window.ethereum.enable();
-        // Wallet is connected, proceed with your logic
-        getUserAccount();
-      } catch (error) {
-        console.error('Error connecting wallet:', error);
-      }
+  const handleConnect = async () => {
+    if (isConnectedd === false) {
+      await connectToMetaMask();
     }
+  };
 
-  async function getUserAccount() {
-  const accounts = await web3.eth.getAccounts();
-  const userAccount = accounts[0];
-  console.log('User account:', userAccount);
-   setUserAccount(userAccount);
-}
+  return (
+    <nav className="w-full items-center px-4 py-4 md:p-4 h-15 bg-[#080d1854] flex justify-between">
+      <div className="text-2xl font-bold text-white shadow-lg shadow-black cursor-pointer">
+        Eth Lucky Draw{" "}
+      </div>
+      <div>
+        <button onClick={handleConnect} className="md:hidden">
+          {" "}
+          <img src={icon} className="h-11" />{" "}
+        </button>
 
-    return(
-        <nav className="w-full items-center px-4 py-4 md:p-4 h-15 bg-[#080d1854] flex justify-between">
-            <div className="text-2xl font-bold text-white shadow-lg shadow-black cursor-pointer">Eth Lucky Draw </div>
-            <div>
-                <button onClick={connectWallet} className="md:hidden"> <img src={icon} className="h-11"/> </button>
-                
-                <button onClick={connectWallet} type="button" className="hidden md:block  text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 rounded-lg text-sm p-3 text-center text-[15px] font-bold me-2 mb-2 px-7">
-                
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 inline-block mx-2 items-center">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
-                </svg>
-
-                    Connect Wallet<br></br> 
-                     {userAccount && (
-                      <div>
-                        <div>{userAccount}</div>
-                      </div>
-      )}             
-                </button></div> 
-
-        </nav>
-    );
+        <button
+          onClick={handleConnect}
+          type="button"
+          className="hidden md:block  text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 rounded-lg text-sm p-3 text-center text-[15px] font-bold me-2 mb-2 px-7"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 inline-block mx-2 items-center"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"
+            />
+          </svg>
+          {connectedAddress
+            ? `${connectedAddress.slice(0, 4)}...${connectedAddress.slice(
+                connectedAddress.length - 4
+              )}`
+            : "Connect"}
+        </button>
+      </div>
+    </nav>
+  );
 }
